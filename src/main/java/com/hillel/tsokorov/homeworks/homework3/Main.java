824cf9fdc320899979f7adf720161a959ff23981
Tsokorov Pavlo <paveltsokorov@gmail.com>
2022-11-11 16:14:04 +0200

    finished homework3

--- src/main/java/com/hillel/tsokorov/homeworks/homework3/Main.java
+++ src/main/java/com/hillel/tsokorov/homeworks/homework3/Main.java
@@ -3,7 +3,6 @@ package com.hillel.tsokorov.homeworks.homework3;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Homework3");
-
+        System.out.println("Homework 3");
     }
 }
