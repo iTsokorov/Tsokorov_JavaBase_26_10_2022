30ff979e61b6aad233e14233b9ba32596c81787a
Tsokorov Pavlo <paveltsokorov@gmail.com>
2022-12-24 13:43:06 +0200

    add homework12

---
+++ src/main/java/com/hillel/tsokorov/homeworks/homework12/Person.java
@@ -0,0 +1,13 @@
+package com.hillel.tsokorov.homeworks.homework12;
+
+public class Person {
+    public static void main(String[] args) {
+        System.out.println(personInfo("Will", "Smith", "New York", "2936729462846"));
+        System.out.println(personInfo("Jackie", "Chan", "Shanghai", "12312412412"));
+        System.out.println(personInfo("Sherlock", "Holmes", "London", "37742123513"));
+    }
+
+    static String personInfo(String firstName, String secondName, String city, String phoneNumber) {
+        return "Зателефонувати громадянинові " + firstName + " " + secondName + " з міста " + city + " можна за номером " + phoneNumber;
+    }
+}
